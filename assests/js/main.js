c338e95a8956c2d7e6278cf2416f31c53ed35693
Kamran Hasanov <kamranhasanov128@gmail.com>
2022-11-12 18:39:23 +0400

    12/11/2022

--- assests/js/main.js
+++ assests/js/main.js
@@ -20,3 +20,24 @@ var swiper = new Swiper(".mySwiper", {
       },
     },
   });
+
+// HOT NFT SCRIPT
+  $(document).ready(function () {
+
+    $(".filter-button").click(function () {
+      var value = $(this).attr('data-filter');
+
+      if (value == "all") {
+        //$('.filter').removeClass('hidden');
+        $('.filter').show('1000');
+      }
+      else {
+        //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+        //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+        $(".filter").not('.' + value).hide('3000');
+        $('.filter').filter('.' + value).show('3000');
+
+      }
+    });
+
+  });
